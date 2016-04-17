59c36e1feea90b980deab38030df0e4eb5a8e6e7
Paul Joannon <hello@pauljoannon.com>
2016-04-17 21:22:10 +0200

    human readable time

--- static/js/datetime.js
+++ static/js/datetime.js
@@ -2,19 +2,23 @@
 * @Author: Paul Joannon <paulloz>
 * @Date:   2016-04-16T09:46:07+02:00
 * @Email:  hello@pauljoannon.com
-* @Last modified by:   paulloz
-* @Last modified time: 2016-04-16T09:59:10+02:00
+* @Last modified by:   Paul Joannon
+* @Last modified time: 2016-04-17T21:21:53+02:00
 */
 
 window.addEventListener('load', function() {
     function update() {
         var now = Date.now();
         [].slice.call(document.querySelectorAll('.time[x-attr-since]')).forEach(function(element) {
-            var timeDiff = now - parseInt(element.getAttribute('x-attr-since'));
-            element.innerText = `${timeDiff}ms`;
+            var timeDiff = (now - parseInt(element.getAttribute('x-attr-since'))) / 1000;
+            if (timeDiff / 60 > 1) {
+                element.innerText = `${parseInt(timeDiff / 60)}min ${parseInt(timeDiff % 60)}s`;
+            } else {
+                element.innerText = `${timeDiff}s`;
+            }
         });
 
-        setTimeout(update, 6000);
+        setTimeout(update, 1000);
     }
 
     update();
